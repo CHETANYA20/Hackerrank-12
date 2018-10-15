0f0e8f2294d946da4ff28370ad749b0ff119fdf4
kasi1996arun <kasi1996arun@gmail.com>
2018-10-15 23:47:47 +0530

    removal of commented unwanted codes

--- Algorithms/Bit-Manipulation/Flipping-bits/Solution.java
+++ Algorithms/Bit-Manipulation/Flipping-bits/Solution.java
@@ -5,36 +5,12 @@ import java.util.*;
 import java.math.BigInteger;
 class Solution
 {
-	/*static long dec(String x)
-	{
-		int v=0;
-		long sum=0;
-		System.out.println(x);
-		for(int i=x.length()-1;i>=0;i--)
-		{
-			System.out.print(i+" : ");
-			int temp=(int)Math.pow(2,v);
-			v++;
-			char ch=x.charAt(i);
-			String t=ch+"";
-			int temp1=(Integer.parseInt(t))*temp;
-			
-			sum=sum+temp1;
-			System.out.println(temp1+" "+sum);
-		}
-		System.out.println(sum);
-		return sum;
-	}*/
 	public static void main(String[] args) {
 		Scanner in=new Scanner(System.in);	
 		int tt=in.nextInt();
 		for(int k=0;k<tt;k++)
 		{
 			long n=in.nextLong();
-			//String x=StringUtils.leftPad(Integer.toBinaryString(n), 32, '0');
-			//binary(n);
-			//System.out.println(x);
-			//System.out.println(Integer.toBinaryString(n));
 			String x=Long.toBinaryString(n);
 			StringBuilder sb=new StringBuilder();
 			for(int i=0;i<(32-x.length());i++)
@@ -42,8 +18,6 @@ class Solution
 				sb.append("0");
 			}
 			sb.append(x);
-			//System.out.println(sb);
-			//char[] ch=sb.toCharArray();
 			for(int i=0;i<32;i++)
 			{
 				if(sb.charAt(i)=='0')
@@ -56,10 +30,8 @@ class Solution
 				}
 			}
 			String x1=sb.toString();
-			//long arun=dec(x1);
 			long abc=new BigInteger(x1,2).longValue();
 			System.out.println(abc);
-			//System.out.println(arun);
 		}
 
 	}
